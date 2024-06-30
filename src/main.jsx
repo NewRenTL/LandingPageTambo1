590150b6ef89de764d783fa1ab675dd1362749c6
NewRenTL <diego.bustamante@utec.edu.pe>
2024-06-30 18:55:59 -0500

    Comment to separate branches

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import BodySection from './layouts/BodySection.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/*Comment to separate branches Main and Develop */}
     <TopBar/>
     <BodySection/>
     <App />
